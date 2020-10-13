d0e8f29a123bf9288fda93a4b23b5301eb2230ca
Patrizio Bekerle <patrizio@bekerle.com>
2020-10-13 17:38:28 +0200

    Implement legacy api

---
+++ src/Controller/LegacyApiController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\ReleaseApi;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LegacyApiController extends AbstractController
+{
+    private $api;
+
+    private $requestStack;
+
+    public function __construct(ReleaseApi $api, RequestStack $requestStack)
+    {
+        $this->api = $api;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @Route("/api/v1/last_release/QOwnNotes/{id}.json")
+     * @param $id
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function lastRelease($id)
+    {
+        if ($id == "macosx") {
+            $id = "macos";
+        }
+
+        $version = $this->requestStack->getCurrentRequest()->get("v");
+        $updateMode = $this->requestStack->getCurrentRequest()->get("um");
+        $debug = $this->requestStack->getCurrentRequest()->get("d");
+        $release = $this->requestStack->getCurrentRequest()->get("r");
+        $os = $this->requestStack->getCurrentRequest()->get("o");
+        $cid = $this->requestStack->getCurrentRequest()->get("cid");
+
+        $filter = [
+            "version" => $version,
+            "um" => $updateMode,
+            "debug" => $debug,
+            "release" => $release,
+            "os" => $os,
+            "cid" => $cid,
+        ];
+
+        $latestRelease = $this->api->fetchLatestRelease($id, $filter);
+
+        $result = [
+            "should_update" => $latestRelease->getNeedUpdate(),
+            "release_version_string" => $latestRelease->getVersion(),
+            "changes_html" => $latestRelease->getReleaseChangesHtml(),
+            "release" => [
+                "assets" => [0 => ["browser_download_url" => $latestRelease->getUrl()]]
+            ]
+        ];
+
+        return $this->json($result);
+    }
+}
