aea96dbcabc21045d71b559c64d8b627d41e88d3
Patrizio Bekerle <patrizio@bekerle.com>
2020-10-18 21:01:41 +0200

    Remove obsolete config and script

--- docker.prod/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Important script that needs to run every n-minutes.
-
-echo "I'm still doing great stuff, and using environment variable example: $EXAMPLE!" >> /var/www/cron.no_environment.log
-. /run/supervisord.env
-echo "I'm still doing great stuff, and using environment variable example: $EXAMPLE!" >> /var/www/cron.sourced_environment.log
